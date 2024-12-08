c4c6167366204764a597fe9e605f424c14427d03
Dale Weber <hybotics@hybridrobotix.io>
2024-12-08 10:14:20 -0800

    Adding

--- discord/kateri.py
+++ discord/kateri.py
@@ -7,6 +7,8 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv('KATERI_DISCORD_TOKEN')
 
+print(f"TOKEN = {TOKEN}")
+
 # Create a new bot client
 intents = discord.Intents.default()
 intents.message_content = True
