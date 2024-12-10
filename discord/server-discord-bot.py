27555492f9ed0cd0b0bb0a46b670826b20960d36
Dale Weber <hybotics@hybridrobotix.io>
2024-12-10 07:46:32 -0800

    Updating daleBot and requirements.txt (to allow upgrading) Adding new server-discord-bot.py - login to server and join channel (NOT working)

--- discord/server-discord-bot.py
+++ discord/server-discord-bot.py
@@ -2,12 +2,13 @@
 import discord
 from discord.ext import commands
 from os import getenv
+import asyncio
 
 #
 #   Get the bot's token from the environment
 #
 TOKEN = getenv('DALEBOT_DISCORD_TOKEN', "Default")
-print(f"TOKEN = '{TOKEN}'")
+#print(f"TOKEN = '{TOKEN}'")
 
 #
 #   Servers and Channels
@@ -26,23 +27,57 @@ SERVER_ID2 = 620838168794497044         #   DevEco
 
 CHANNEL_ID2_1 = 620838168794497046      #   #chat
 
-#   Create a new bot instance with a specified command prefix
-intents = discord.Intents.default()
-intents.typing = False
-intents.presences = False
+# Create a new bot instance with a specified command prefix
+INTENTS = discord.Intents.default()
+INTENTS.typing = False
+INTENTS.presences = False
+INTENTS.members = True
+INTENTS.messages = True
+INTENTS.guilds = True
 
-bot = commands.Bot(command_prefix='!', intents=intents)
+bot = commands.Bot(command_prefix='!', intents=INTENTS)
 
 @bot.event
 async def on_ready():
-    """Event triggered when the bot is ready"""
-    print(f"Logged in as {bot.user}")
+  """Event triggered when the bot is ready"""
+  print(f"Logged in as {bot.user}")
 
-#   Command to make the bot say hello
-@bot.command(name="hello")
+#
+# Bot Commands
+#
+
+# Command: Hello
+@bot.command(name='hello')
 async def hello(ctx):
-    """Says hello back to the user"""
-    await ctx.send("Hello!")
+  """Greets the user"""
+  await ctx.send(f'Hello {ctx.author.name}!')
+
+# Command: Ping
+@bot.command(name='ping')
+async def ping(ctx):
+  """Responds with the bot's latency"""
+  await ctx.send(f'Pong! Latency: {round(bot.latency * 1000)}ms')
+
+# Command: Clear messages
+@bot.command(name='clear')
+@commands.has_permissions(manage_messages=True)
+async def clear(ctx, amount=5):
+  """Clears specified number of messages"""
+  await ctx.channel.purge(limit=amount + 1)
+  await ctx.send(f'Cleared {amount} messages!', delete_after=5)
+
+# Command: Member count
+@bot.command(name='members')
+async def members(ctx):
+  """Shows the server member count"""
+  await ctx.send(f'This server has {ctx.guild.member_count} members!')
+
+#   Command to make the bot say goodbye
+@bot.command(name="bye")
+async def goodbye(ctx):
+  """Says bye and logs off"""
+  await ctx.send("Goodbye!")
+  await bot.close()
 
 #   Command to tell the user about the bot's help command
 #@bot.command(name="help")
@@ -52,41 +87,43 @@ async def hello(ctx):
 #    embed.add_field(name="!hello", value="Say hello back to the user")
 #    await ctx.send(embed=embed)
 
-#   Command to make the bot say goodbye
-@bot.command(name="bye")
-async def goodbye(ctx):
-    """Says bye and logs off"""
-    await ctx.send("Goodbye!")
-    await bot.close()
-
 #
 # Function to make the bot join a server
 #
-def join_server(server_id, channel_id):
-    #
-    # Create a new session with the Discord API
-    #
-    session = discord.client()
+def join_server(server_id, channel_id, intents):
+  #
+  # Create a new session with the Discord API
+  #
+  session = discord.Client(intents=intents)
 
-    # Fetch the server
-    server = session.get_guild(server_id)
+  # Fetch the server
+  server = session.get_guild(server_id)
 
-    if not server:
-        print(f'Server {server_id} not found')
-        return
+  if not server:
+    print(f'Server {server_id} not found')
+    return
 
-    # Join the channel
-    channel = server.get_channel(channel_id)
+  # Join the channel
+  channel = server.get_channel(channel_id)
 
-    if not channel:
-        print(f'Channel {channel_id} not found in server {server_id}')
-        return
+  if not channel:
+    print(f'Channel {channel_id} not found in server {server_id}')
+    return
 
-    # Add the bot to the channel's permissions
-    channel.set_permissions(bot, send_messages=True)
-    channel.set_permissions(bot, add_reactions=True)
+  # Add the bot to the channel's permissions
+  channel.set_permissions(bot, send_messages=True)
+  channel.set_permissions(bot, add_reactions=True)
 
-join_server(SERVER_ID1, CHANNEL_ID1_1)
+# Run the bot
+def start(server, channel, intents):
+  TOKEN = getenv('DALEBOT_DISCORD_TOKEN', "INVALID")
 
-# Run the bot with your token
-bot.run(TOKEN)
+  if TOKEN == "INVALID":
+    print(f"Bot token is not valid!")
+  else:
+    join_server(server, channel, intents)
+    bot.run(TOKEN)
+
+if __name__ == "__main__":
+  # Hybrid Robotics Community, #bot-commands
+  start(SERVER_ID1, CHANNEL_ID1_1, INTENTS)
